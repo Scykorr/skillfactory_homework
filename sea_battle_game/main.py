42d6793f34b3f4caede199db2c46413d13e3ce80
Anton Fedosov <fedosov-toxa@mail.ru>
2023-10-16 17:19:37 +0300

    add classes Gamer, GameBoard, Errors, GameEvent

--- sea_battle_game/main.py
+++ sea_battle_game/main.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Coord:
     """
     Класс, описывающий координату на игровом поле
@@ -62,10 +65,6 @@ class Ship:
     def pos(self):
         return self.__positions
 
-    @pos.setter
-    def pos(self, pos):
-        self.__positions = pos
-
     def _set_name(self, _pos):
         if len(_pos) == 1:
             self.__name = 'Подводная лодка'
@@ -75,16 +74,102 @@ class Ship:
             self.__name = 'Крейсер'
 
 
-class GameGraphic:
+class Gamer:
+
+    def __init__(self, name):
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+
+class GameBoard:
+
+    def __init__(self, ships, coords):
+        pass
+
+
+class Errors(Exception):
+
     def __init__(self):
         pass
 
-    def draw_field(self):
-        print()
 
+class GameLogic:
+
+    def __init__(self):
+        self.ships = []
+        self.coordinates = []
+
+class GameEvent:
+    # пустое событие
+    Event_None = 0
+    # событие таймера
+    Event_Tick = 1
+    # событие "выстрела" по цели
+    Event_Hit = 2
+
+    def __init__(self, type, data) -> None:
+        self.type = type
+        self.data = data
+
+    def getType(self):
+        return self.type
+
+    def getData(self):
+        return self.data
+
+class GameGraphic:
+
+    def __init__(self, w, h, logic) -> None:
+        self.main_w = w
+        self.main_h = h
+        self.logic = logic
+
+    def draw_field(self):
+        draw_list = [[0 for _ in range(6)] for _ in range(6)]
+        for el in draw_list:
+            print(*el)
+
+        # тут будет цикл обработки сообщений и взаимодействия с пользователем
+    def run(self):
+        running = True
+        while running:
+            # сначала посылаем сообщение Event_Tick, чтобы была сгенерирована цель
+            self.processEvent(GameEvent(GameEvent.Event_Tick, None))
+
+            # отрисовываем то, что сейчас должно быть на доске
+            self.draw()
+
+            # запрашиваем команды у пользователя
+            print('------------------')
+            print('0. exit')
+            print('1. hit target')
+            cmd = int(input())
+
+            # значение пустого события по умолчанию
+            event = GameEvent(GameEvent.Event_None, None)
+            # если команда 0 - идем на выход
+            if cmd == 0:
+                running = False
+                continue
+            # если команда 1 запрашиваем координаты
+            if cmd == 1:
+                x = int(input('input X: '))
+                y = int(input('input Y: '))
+                event = GameEvent(GameEvent.Event_Hit, Pos(x, y))
+
+            # отрабатываем событие
+            self.processEvent(event)
+
+    def processEvent(self, event):
+        self.logic.processEvent(event)
 
 if __name__ == "__main__":
     first_coord = Coord()
     first_coord.x_coord = 5
     first_coord.y_coord = 6
-    first_ship = Ship()
+    # first_ship = Ship()
+    game = GameGraphic()
+    game.draw_field()
