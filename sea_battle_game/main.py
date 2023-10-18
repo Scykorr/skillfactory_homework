4666574d1193901b0b5c2bf0e3c8c83fdda79e6c
Anton Fedosov <fedosov-toxa@mail.ru>
2023-10-18 17:22:48 +0300

    do docstring main, players

--- sea_battle_game/main.py
+++ sea_battle_game/main.py
@@ -1,35 +1,99 @@
+"""
+Основной файл игры "Морской бой"
+"""
+
 from random import randint
 from my_exceptions import BoardWrongShipException
 from players import Computer, User
 from main_elements import GameBoard, Ship, Coord
+from typing import Optional
 
 
 class Game:
-    def __init__(self, size=6):
-        self.size = size
-        user_board = self.get_board()
-        comp_board = self.get_board()
-        comp_board.hid = True
+    """
+    Класс игры.
 
-        self.computer = Computer(comp_board, user_board)
-        self.user = User(user_board, comp_board)
+    ...
 
-    def get_board(self):
-        board = None
+    Attributes
+    ----------
+        size : int
+            размер игрового поля
+        user_board : GameBoard
+            объект игровой доски пользователя
+        comp_board : GameBoard
+            объект игровой доски компьютера
+        computer : Computer
+            объект игрока компьютера
+        user : User
+            объект игрока человека
+
+    Methods
+    -------
+        get_board(self) -> GameBoard:
+            Метод, возвращающий игровое поле.
+        random_place(self) -> Optional[GameBoard]:
+            Метод, размещающий корабли на игровом поле
+        get_first_page(self) -> None:
+            Метод, выводящий приветственное сообщение
+        play_game(self) -> None:
+            Метод, реализующий основные действия игры с проверкой выигрыша участников
+        start(self) -> None:
+            Метод, запускающий игру
+    """
+
+    def __init__(self, size: int = 6) -> None:
+        """
+        Конструктор класса игры.
+
+        Parameters
+        ----------
+            size : int
+                размер игрового поля
+        Returns
+        -------
+            None
+        """
+        self.size: int = size
+        self.user_board: GameBoard = self.get_board()
+        self.comp_board: GameBoard = self.get_board()
+        self.comp_board.hid = True
+
+        self.computer: Computer = Computer(self.comp_board, self.user_board)
+        self.user: User = User(self.user_board, self.comp_board)
+
+    def get_board(self) -> GameBoard:
+        """
+        Метод, возвращающий игровое поле.
+
+        Returns
+        -------
+            board : GameBoard
+                объект класса игрового поля
+        """
+        board: Optional[GameBoard] = None
         while board is None:
-            board = self.random_place()
+            board: GameBoard = self.random_place()
         return board
 
-    def random_place(self):
-        ship_lens = [3, 2, 2, 1, 1, 1, 1]
-        board = GameBoard(size=self.size)
-        attempts = 0
+    def random_place(self) -> Optional[GameBoard]:
+        """
+        Метод, размещающий корабли на игровом поле.
+
+        Returns
+        -------
+            board : Optional[GameBoard]
+                объект класса игрового поля
+        """
+        ship_lens: list = [3, 2, 2, 1, 1, 1, 1]
+        board: GameBoard = GameBoard(size=self.size)
+        attempts: int = 0
         for len_ship in ship_lens:
             while True:
                 attempts += 1
                 if attempts > 2000:
                     return None
-                ship = Ship(Coord(randint(0, self.size), randint(0, self.size)), len_ship, randint(0, 1))
+                ship: Ship = Ship(Coord(randint(0, self.size), randint(0, self.size)), len_ship, randint(0, 1))
                 try:
                     board.add_ship(ship)
                     break
@@ -38,7 +102,14 @@ class Game:
         board.begin()
         return board
 
-    def get_first_page(self):
+    def get_first_page(self) -> None:
+        """
+        Метод, выводящий приветственное сообщение.
+
+        Returns
+        -------
+            None
+        """
         self.user.name = input('Игра - Морской Бой\n'
                                'Введите Ваш игровой nickname: ')
         print(f"\nПриветсвуем Вас, {self.user.name}!\n\n"
@@ -47,8 +118,15 @@ class Game:
               f" y - номер столбца\n")
         input('Для начала игры нажмите клавишу "Enter"\n')
 
-    def play_game(self):
-        num = 0
+    def play_game(self) -> None:
+        """
+        Метод, реализующий основные действия игры с проверкой выигрыша участников.
+
+        Returns
+        -------
+            None
+        """
+        num: int = 0
         while True:
             print("-" * 20)
             print(f"Доска {self.user.name}:")
@@ -59,11 +137,11 @@ class Game:
             if num % 2 == 0:
                 print("-" * 20)
                 print(f"Ходит {self.user.name}!")
-                repeat = self.user.move()
+                repeat: bool = self.user.move()
             else:
                 print("-" * 20)
                 print("Ходит компьютер!")
-                repeat = self.computer.move()
+                repeat: bool = self.computer.move()
             if repeat:
                 num -= 1
 
@@ -78,7 +156,14 @@ class Game:
                 break
             num += 1
 
-    def start(self):
+    def start(self) -> None:
+        """
+        Метод, запускающий игру.
+
+        Returns
+        -------
+            None
+        """
         self.get_first_page()
         self.play_game()
 
--- sea_battle_game/players.py
+++ sea_battle_game/players.py
@@ -1,17 +1,68 @@
-from main_elements import Coord
+from main_elements import Coord, GameBoard
 from random import randint
 from my_exceptions import OutBoardError, CellRepeatError
+from typing import Callable, Optional, Any
 
 
 class Player:
-    def __init__(self, board, enemy):
+    """
+    Базовый класс игрока
+
+    ...
+
+    Attributes
+    ----------
+        __board : GameBoard
+            собственное игровое поле
+        __enemy : GameBoard
+            игровое поле соперника
+
+    Methods
+    -------
+        ask(self) -> None:
+            Метод, возвращающий исключение NotImplementedError
+        move(self) -> bool:
+            Метод, отвечающий за попадание игрока в клетку.
+        board(self) -> GameBoard:
+            Геттер объекта "игровое поле" игрока.
+
+    """
+    def __init__(self, board, enemy) -> None:
+        """
+        Конструктор базового класса игрока.
+
+        Parameters
+        ----------
+            board : GameBoard
+                собственное игровое поле
+            enemy : GameBoard
+                игровое поле соперника
+        Returns
+        -------
+            None
+        """
         self.__board = board
         self.__enemy = enemy
 
-    def ask(self):
+    def ask(self) -> None:
+        """
+        Метод, возвращающий исключение NotImplementedError.
+
+        Returns
+        -------
+            None
+        """
         raise NotImplementedError()
 
-    def move(self):
+    def move(self) -> bool:
+        """
+        Метод, отвечающий за попадание игрока в клетку.
+
+        Returns
+        -------
+            repeat : bool
+                проверка успешного попадания
+        """
         while True:
             try:
                 target = self.ask()
@@ -23,34 +74,127 @@ class Player:
                 print(e)
 
     @property
-    def board(self):
+    def board(self) -> GameBoard:
+        """
+        Геттер объекта "игровое поле" игрока.
+
+        Returns
+        -------
+            __board : GameBoard
+                объект игрового поля игрока
+        """
         return self.__board
 
 
 class Computer(Player):
-    def ask(self):
+    """
+    Класс игрока Компьютер. Родитель - Player.
+
+    ...
+
+    Attributes
+    ----------
+
+    Methods
+    -------
+    ask(self) -> Coord:
+        Переопределенный метод, возвращающий объект Coord, отвечающий за координаты игрового поля компьютера.
+
+    """
+    def ask(self) -> Coord:
+        """
+        Переопределенный метод, возвращающий объект Coord, отвечающий за координаты игрового поля компьютера.
+
+        Returns
+        -------
+            comp_coord : Coord
+                объект класса Coord, отвечающий за координаты игрового поля.
+
+        """
         comp_coord = Coord(randint(0, 5), randint(0, 5))
         print(f"Ход компьютера: {comp_coord.x_coord + 1} {comp_coord.y_coord + 1}")
         return comp_coord
 
 
 class User(Player):
+    """
+    Класс игрок-человек. Родитель - Player.
 
+    ...
+
+    Attributes
+    ----------
+        __name : str
+            игровое имя человека
+    Methods
+    -------
+        name(self) -> str:
+            Геттер имени игрока-человека.
+        name(self, _name) -> None:
+            Сеттер имени игрока-человека.
+        ask(self) -> Coord:
+            Переопределенный метод, возвращающий объект Coord, отвечающий за координаты игрового поля игрока-человека.
+
+
+    """
     def __init__(self, board, enemy):
+        """
+        Конструктор класса игрок-человек.
+
+        Parameters
+        ----------
+            board : GameBoard
+                собственное игровое поле
+            enemy : GameBoard
+                игровое поле соперника
+
+        Returns
+        -------
+            None
+        """
         super().__init__(board, enemy)
         self.__name = 'User'
 
     @property
-    def name(self):
+    def name(self) -> str:
+        """
+        Геттер имени игрока-человека.
+
+        Returns
+        -------
+            __name : str
+                имя игрока-человека
+        """
         return self.__name
 
     @name.setter
-    def name(self, _name):
+    def name(self, _name) -> None:
+        """
+        Сеттер имени игрока-человека.
+
+        Parameters
+        ----------
+            _name : str
+                имя игрока-человека
+
+        Returns
+        -------
+            None
+        """
         self.__name = _name
 
-    def ask(self):
+    def ask(self) -> Coord:
+        """
+        Переопределенный метод, возвращающий объект Coord, отвечающий за координаты игрового поля игрока-человека.
+
+        Returns
+        -------
+            comp_coord : Coord
+                объект класса Coord, отвечающий за координаты игрового поля.
+
+        """
         while True:
-            coords = input("Ваш ход: ").split()
+            coords: list = input("Ваш ход: ").split()
 
             if len(coords) != 2:
                 print(" Введите 2 координаты! ")
