e9567b28efc0f32bd9f9c7b2e08469a200ad78f9
Anton Fedosov <fedosov-toxa@mail.ru>
2023-11-01 09:43:58 +0300

    docstring is done

--- telegramm_bot/extensions.py
+++ telegramm_bot/extensions.py
@@ -8,13 +8,48 @@ load_dotenv(find_dotenv())
 
 
 class APIException(Exception):
+    """
+    Класс исключений ошибок пользователя.
+
+    ...
+
+    """
     pass
 
 
-class CryptoConverter:
-    @staticmethod
-    def get_price(base: str, quote: str, amount: str):
+class CurrencyConverter:
+    """
+    Класс конвертера валют.
 
+    ...
+
+
+    Methods
+    -------
+    @staticmethod
+    get_price(base: str, quote: str, amount: str) -> float:
+        Метод, конвертирующий валюту.
+    """
+
+    @staticmethod
+    def get_price(base: str, quote: str, amount: str) -> float:
+        """
+        Метод, конвертирующий валюту.
+
+        Parameters
+        ----------
+            quote : str
+                валюта из которой конвертируем
+            base : str
+                валюта в которую конвертируем
+            amount : int
+                количество конвертируемой валюты
+
+        Returns
+        -------
+            total_quote : float
+                итоговое количество конвертируемой валюты
+        """
         if quote == base:
             raise APIException(f'Невозможно перевести одинаковые валюты: {base}.')
 
@@ -33,6 +68,7 @@ class CryptoConverter:
         except ValueError:
             raise APIException(f'Не удалось обработать количество: {amount}')
 
-        main_request = requests.get(f'https://v6.exchangerate-api.com/v6/{os.getenv('api_token')}/pair/{base_ticker}/{quote_ticker}')
+        main_request = requests.get(
+            f'https://v6.exchangerate-api.com/v6/{os.getenv('api_token')}/pair/{base_ticker}/{quote_ticker}')
         total_quote = float(json.loads(main_request.content)['conversion_rate'] * amount)
         return total_quote
--- telegramm_bot/main.py
+++ telegramm_bot/main.py
@@ -1,40 +1,89 @@
 import telebot
 from config import currencies
-from extensions import APIException, CryptoConverter
+from extensions import APIException, CurrencyConverter
 import os
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
 
-def main():
+def main() -> None:
+    """
+    Основная функция работы телеграм-бота.
+
+        Parameters:
+
+
+        Returns:
+            None
+    """
     bot = telebot.TeleBot(os.getenv('telegram_token'))
 
     @bot.message_handler(commands=['start', 'help'])
-    def handle_start_help(message: telebot.types.Message):
+    def handle_start_help(message: telebot.types.Message) -> None:
+        """
+        Функция, выдающая по запросу справочную информацию о работе бота.
+
+            Parameters:
+                message : telebot.types.Message
+                    сообщение пользователя
+
+            Returns:
+                None
+        """
         text = ('Данные вводятся в формате:\n<1>\' \'<2>\' \'<3>'
-                '\n"1" - имя валюты, цену которой хотите узнать'
-                '\n"2" - имя валюты, в которой надо узнать цену первой валюты'
-                '\n"3" - количество первой валюты'
+                '\n"1" - имя валюты, из какой конвертируем'
+                '\n"2" - имя валюты, в которую конвертируем'
+                '\n"3" - количество конвертируемой волюты'
                 '\n\' \' - пробел\n'
                 '\nСписок доступных команд: /commands')
         bot.reply_to(message, text)
 
     @bot.message_handler(commands=['commands'])
-    def handle_check_commands(message: telebot.types.Message):
+    def handle_check_commands(message: telebot.types.Message) -> None:
+        """
+        Функция, выдающая по запросу список команд бота.
+
+            Parameters:
+                message : telebot.types.Message
+                    сообщение пользователя
+
+            Returns:
+                None
+        """
         text = ('Доступные комманды:\n'
                 '/values - список всех доступных валют')
         bot.reply_to(message, text)
 
     @bot.message_handler(commands=['values'])
-    def handle_check_currencies(message: telebot.types.Message):
+    def handle_check_currencies(message: telebot.types.Message) -> None:
+        """
+        Функция, выдающая по запросу список доступных валют.
+
+            Parameters:
+                message : telebot.types.Message
+                    сообщение пользователя
+
+            Returns:
+                None
+        """
         output_text = 'Доступные валюты:'
         for key in currencies.keys():
             output_text = '\n'.join((output_text, key,))
         bot.reply_to(message, output_text)
 
     @bot.message_handler(content_types=['text', ])
-    def covert_currencies(message: telebot.types.Message):
+    def covert_currencies(message: telebot.types.Message) -> None:
+        """
+        Функция вывода конвертированной валюты.
+
+            Parameters:
+                message : telebot.types.Message
+                    сообщение пользователя
+
+            Returns:
+                None
+        """
         try:
             values = message.text.split(' ')
 
@@ -44,7 +93,7 @@ def main():
             base, quote, amount = values
             base = base.lower()
             quote = quote.lower()
-            total_base = CryptoConverter.get_price(base, quote, amount)
+            total_base = CurrencyConverter.get_price(base, quote, amount)
         except APIException as e:
             bot.reply_to(message, f'Ошибка пользователя.\n{e}')
         except Exception as e:
